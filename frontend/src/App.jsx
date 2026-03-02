75e8fb0607fd1a8499883bd78d3d916e88c05da2
HencoSchutte <37823949@student.g.nwu.ac.za>
2026-03-02 03:13:07 +0200

    Fix component import cases for Linux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Navbar from "./components/navbar";
-import Loader from "./components/Loader";
+import Loader from "./components/loader";
 import ProductPage from "./pages/ProductPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
